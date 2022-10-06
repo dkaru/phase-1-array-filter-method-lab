8ee908b27116c2d66938eaa1762c8946de81bbc5
Moringa School <dwangui799@gmail.com>
2022-10-06 11:49:08 +0300

    complited assighnment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivernames, firstname){
+ const names =  drivernames.filter(function (item){
+return item .toLowerCase() === firstname.toLowerCase ()
+
+
+ } )
+ return names
+}
+function fuzzyMatch(drivernames, letters,) { 
+    const  filteredArrays =drivernames.filter(function (names){
+        return names.substring(0,2) === letters 
+    } )
+    return filteredArrays
+}
+function matchName(drivernames, names){
+    const filteredArrays = drivernames.filter(function (list){
+   return  list.name === names;    
+    } )
+return filteredArrays
+}
+
+
+
+
+
+
+
+
